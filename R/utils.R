4d0f42d43f5431493cfc90de4267f738c739f564
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2020-12-01 18:37:58 +0100

    remove dependencies to mrgsolve, purrr and rlang

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,4 @@
-#' @importFrom purrr map2
-#' @importFrom mrgsolve dmat
-#' @importFrom dplyr across as_tibble everything mutate row_number pull
-#' @importFrom tidyr unite
+#' @importFrom dplyr across everything mutate row_number pull
 #' @importFrom magrittr add multiply_by raise_to_power %>%
-#' @importFrom rlang .data
+#' @importFrom tidyr unite
 NULL
