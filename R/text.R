53deed24492cb8c5c56e40629905e6a0a4799728
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2020-12-02 18:07:06 +0100

    refactor text omegap

--- R/text.R
+++ R/text.R
@@ -1,9 +1,9 @@
-#' @param t_val output of get_t_val
+#t_val output of get_t_val
 text_tp <- function(t_val){
   paste0("\n$THETAP (Prior on THETA values)\n", paste0(t_val, " FIXED ;  ", seq_len(length(t_val)), " ", names(t_val), collapse = "\n"), "\n", collapse = "\n")
 }
 
-#' @param t_var output of get_t_var
+#t_var output of get_t_var
 text_tpv <- function(t_var){
   tvar <- get_t_var(c(KA = .1, CL = .2, V = .3), "SE")
   ntvar <- ""
@@ -17,4 +17,8 @@ text_tpv <- function(t_var){
     pull() %>%
     paste0(collapse = "\n")
   paste0("\n$THETAPV (Variance-covariance of THETA)\n", txttvar, "\n")
+}
+
+text_op <- function(o_val){
+  paste0("\n$OMEGAP (Prior on OMEGA values)\n", o_val %>% paste0(" FIXED ;  ",names(o_val),collapse = "\n"), "\n")
 }
