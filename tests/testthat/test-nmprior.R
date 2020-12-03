162d6d2cc1ef505d9ac0883ce996845eab32ec6a
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2020-12-03 17:27:30 +0100

    test

--- tests/testthat/test-nmprior.R
+++ tests/testthat/test-nmprior.R
@@ -3,8 +3,8 @@ test_that("nmprior works", {
                               THETA_VALUE = list(35.5,23.4,22.2,12),
                               THETA_PRECISION = list(1.4, 1, 1.44, 5.33),
                               OMEGA_VALUE = list(0.149, 0.234),
+                              OMEGA_TYPE = "CV",
                               OMEGA_PRECISION = list(0.076, 0.075),
-                              OMEGA2_or_CV = "CV",
                               PRECISION = "SE"
   ))
 })
